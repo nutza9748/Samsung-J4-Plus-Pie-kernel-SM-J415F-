1d0e4a61c59fcdecfb5e67571cd8d5f454d3c8a6
Kopra159 <48631763+Kopra159@users.noreply.github.com>
2019-07-05 18:59:55 +0200

    Update build_kernel.sh

--- build_kernel.sh
+++ build_kernel.sh
@@ -5,7 +5,7 @@ export PATH=$Pathtotoolcain/arm-linux-androideabi-4.9-brillo-m7-dev/bin:$PATH
 export ARCH=arm
 [ -d ${OUT_DIR} ] && rm -rf ${OUT_DIR}
 mkdir ${OUT_DIR}
-make clean $OUT_DIR && make mrproper $OUT_DIR
+make clean ${OUT_DIR} && make mrproper ${OUT_DIR}
 make ${COMMON_ARGS} j4primelte_sea_open_defconfig
 //make ${COMMON_ARGS} menuconfig 
 make -j$(nproc --all) ${COMMON_ARGS} 
